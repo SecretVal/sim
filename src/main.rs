a4238124a05e1bcadb9f2e192a1fb4c9fed89587
secretval <108518296+SecretVal@users.noreply.github.com>
2024-09-01 14:43:11 +0200

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#![allow(dead_code)]
+use std::io::{stdout, Result};
+
+use crossterm::{
+    cursor::{MoveDown, MoveLeft, MoveRight, MoveTo, MoveUp},
+    event::{read, Event, KeyCode},
+    execute, style,
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen},
+};
+
+#[derive(Debug, Default)]
+struct State {
+    mode: Mode,
+}
+
+#[derive(Debug, Default)]
+enum Mode {
+    #[default]
+    Normal,
+    Insert,
+}
+
+fn main() -> Result<()> {
+    let mut state = State::default();
+    enable_raw_mode()?;
+    let mut stdout = stdout();
+    execute!(
+        stdout,
+        Clear(ClearType::All),
+        MoveTo(0, 0),
+        EnterAlternateScreen
+    )?;
+
+    let mut quit = false;
+    while !quit {
+        let event = read()?;
+        match state.mode {
+            Mode::Normal => match event {
+                Event::Key(e) => match e.code {
+                    KeyCode::Char('q') => quit = true,
+                    KeyCode::Char('h') => execute!(stdout, MoveLeft(1))?,
+                    KeyCode::Char('j') => execute!(stdout, MoveDown(1))?,
+                    KeyCode::Char('k') => execute!(stdout, MoveUp(1))?,
+                    KeyCode::Char('l') => execute!(stdout, MoveRight(1))?,
+                    KeyCode::Char('i') => state.mode = Mode::Insert,
+                    _ => todo!(),
+                },
+                _ => todo!(),
+            },
+            Mode::Insert => {
+                let mut esc = false;
+                while !esc {
+                    let event = read()?;
+                    match event {
+                        Event::Key(key) => match key.code {
+                            KeyCode::Esc => esc = true,
+                            KeyCode::Backspace => {
+                                execute!(stdout, MoveLeft(1), style::Print(" "), MoveLeft(1))?
+                            }
+                            KeyCode::Char(e) => execute!(stdout, style::Print(e))?,
+                            _ => todo!(),
+                        },
+                        _ => todo!(),
+                    }
+                }
+                state.mode = Mode::Normal;
+            }
+        }
+    }
+    disable_raw_mode()?;
+    Ok(())
+}
